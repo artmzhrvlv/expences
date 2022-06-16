491bd72e65f36849e53ddaf37a0652173a14ac46
Artyom Zhuravlev <SantaD3zert@gamil.com>
2022-06-16 17:36:16 +0300

    first commit

---
+++ ruby.rb
@@ -0,0 +1,25 @@
+require "rexml/document"
+require "date"
+
+current_path = File.dirname(__FILE__)
+file_name = current_path + "/ex1.xml"
+
+file = File.new(file_name)
+
+doc = REXML::Document.new(file)
+
+h = []
+f = []
+
+doc.elements.each("expences/expence") do |item|
+  loss_sum = item.attributes["amount"].to_i
+  loss_date = Date.parse(item.attributes["date"])
+
+  h.unshift(loss_sum)
+  f.unshift(item.attributes)
+end
+
+file.close
+
+p h
+p f
